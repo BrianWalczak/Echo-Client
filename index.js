068d60b7153c628bedf4fa0b7d0affa35c2551a1
Brian Walczak <72092838+BrianWalczak@users.noreply.github.com>
2023-12-13 09:32:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,46 @@ const readline = require('readline');
 const fs = require('fs');
 var config = null;
 const startReadline = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 function updateConfig() {
-  return new Promise((resolve, reject) => {
-    fs.readFile('config.json', 'utf8', function(err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        config = JSON.parse(data);
-        resolve();
-      }
+    return new Promise((resolve, reject) => {
+        fs.readFile('config.json', 'utf8', function(err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                config = JSON.parse(data);
+                resolve();
+            }
+        });
     });
-  });
 }
 
 async function run() {
-	await updateConfig();
+    await updateConfig();
 
-	const command = await prompt({
-    type: 'select',
-    name: 'type',
-    message: 'Please select an option:',
-    choices: ['View Channel', 'Configuration', 'Download Image'],
-  });
+    const command = await prompt({
+        type: 'select',
+        name: 'type',
+        message: 'Please select an option:',
+        choices: ['View Channel', 'Configuration', 'Download Image'],
+    });
 
-	startReadline.close();
-	console.clear();
-	if(command.type == "View Channel") {
-		import('./app.js');
-	}
+    startReadline.close();
+    console.clear();
+    if (command.type == "View Channel") {
+        import('./app.js');
+    }
 
-	if(command.type == "Configuration") {
-		import('./config.js');
-	}
+    if (command.type == "Configuration") {
+        import('./config.js');
+    }
 
-	if(command.type == "Download Image") {
-		import('./download.js');
-	}
+    if (command.type == "Download Image") {
+        import('./download.js');
+    }
 }
 
 console.clear();
